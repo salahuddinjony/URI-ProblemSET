6f636892083669158c6577d23d8c55410190897c
Salah Uddin <salahuddinjony>
2022-06-18 02:30:32 +0600

    Added problem

---
+++ AreaOf_Circle.cpp
@@ -0,0 +1,20 @@
+#include <iomanip>  // std::setprecision
+#include<iostream>
+using namespace std; 
+int main(){
+double R,A;
+cin>>R;
+A= 3.14159 * (R*R);
+cout<<"A="<<fixed<<setprecision(4)<<A<<endl;
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ Extramely_Bacis.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int A,B;
+cin>>A>>B;
+int X= A+B;
+
+cout<<"X = "<<X<<endl;
+
+
+}
+
+
+
+
+
+
